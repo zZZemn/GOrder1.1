f646dc0578ced1a3de4ccba395524aab5148270c
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-05-08 10:14:51 +0800

    validation

--- process/delivered-add-process.php
+++ process/delivered-add-process.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 session_start();
 
@@ -11,7 +11,7 @@ if (isset($_SESSION['id'])) {
     $emp = $result->fetch_assoc();
 }
 
-if(isset($_POST['add_delivered'])) {
+if (isset($_POST['add_delivered'])) {
 
     $inv_id = mt_rand(100000, 999999);
     $check_inv_id = "SELECT * FROM inventory WHERE INV_ID = $inv_id";
@@ -31,42 +31,43 @@ if(isset($_POST['add_delivered'])) {
 
     $qty = $del_qty;
 
-    $check_pro_price = "SELECT * FROM products WHERE PRODUCT_ID = $product_id";
-    // if($conn->query($check_pro_price) === TRUE)
-    // {
+    if (is_numeric($product_id)) {
+        $check_pro_price = "SELECT * FROM products WHERE PRODUCT_ID = $product_id";
         $check_pro_price_result = $conn->query($check_pro_price);
-        if($check_pro_price_result->num_rows > 0)
-        {
-            $product = $check_pro_price_result->fetch_assoc();
-            $selling_price = $product['SELLING_PRICE'];
+        if ($check_pro_price_result !== false) {
+            if ($check_pro_price_result->num_rows > 0) {
+                $product = $check_pro_price_result->fetch_assoc();
+                $selling_price = $product['SELLING_PRICE'];
 
-            $mark_up = $selling_price - $supp_price; 
-        }
-        else {
-            $mark_up = 0;
-        }
+                $mark_up = $selling_price - $supp_price;
+            } else {
+                header("Location: ../admin/delivered-products.php?del_id=$del_id&status=failed&message=product_not_found");
+                exit;
+            }
 
-        $insert_new_delivered = "INSERT INTO `inventory`(`INV_ID`, `DELIVERY_ID`, `PRODUCT_ID`, `SUPPLIER_PRICE`, `QUANTITY`, `EXP_DATE`, `DEL_QUANTITY`, `MARK_UP`) 
-                                                VALUES ('$inv_id','$del_id','$product_id','$supp_price','$qty','$expiration_date','$del_qty','$mark_up')";
+            $insert_new_delivered = "INSERT INTO `inventory`(`INV_ID`, `DELIVERY_ID`, `PRODUCT_ID`, `SUPPLIER_PRICE`, `QUANTITY`, `EXP_DATE`, `DEL_QUANTITY`, `MARK_UP`) 
+                                    VALUES ('$inv_id','$del_id','$product_id','$supp_price','$qty','$expiration_date','$del_qty','$mark_up')";
 
-        $del_price = $supp_price * $del_qty;
+            $del_price = $supp_price * $del_qty;
 
-        $update_del_price = "UPDATE `delivery` SET `DELIVERY_PRICE`= DELIVERY_PRICE + $del_price WHERE DELIVERY_ID = $del_id";
+            $update_del_price = "UPDATE `delivery` SET `DELIVERY_PRICE`= DELIVERY_PRICE + $del_price WHERE DELIVERY_ID = $del_id";
 
-        if($conn->query($insert_new_delivered) === TRUE && $conn->query($update_del_price) === TRUE){
-            header("Location: ../admin/delivered-products.php?del_id=$del_id&status=success");
-            exit;
+            if ($conn->query($insert_new_delivered) === TRUE && $conn->query($update_del_price) === TRUE) {
+                header("Location: ../admin/delivered-products.php?del_id=$del_id&status=success");
+                exit;
+            } else {
+                header("Location: ../admin/delivered-products.php?del_id=$del_id&status=failed");
+                exit;
+            }
         } else {
-            header("Location: ../admin/delivered-products.php?del_id=$del_id&status=failed");
+            header("Location: ../admin/delivered-products.php?del_id=$del_id&status=product_id_not_exist");
             exit;
         }
+    } else {
+        header("Location: ../admin/delivered-products.php?del_id=$del_id&status=product_id_not_exist");
+        exit;
     }
-    // else
-    // {
-    //     header("Location: ../admin/delivered-products.php?del_id=$del_id&status=product_id_not_exist");
-    //     exit;
-    // }
-else {
+} else {
     header("Location: ../admin/delivered-products.php?status=failed");
     exit;
 }
