1c4c45aab5aa91057e9f935314a9fb8192cae560
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-05-10 14:17:45 +0800

    edit product

--- database/db.php
+++ database/db.php
@@ -1,8 +1,13 @@
 <?php 
+// $dbservername = "localhost";
+// $dbusername = "u711816221_goldengate";
+// $dbpassword = "Pabiligamot10";
+// $dbname = "u711816221_gorder";
+
 $dbservername = "localhost";
-$dbusername = "u711816221_goldengate";
-$dbpassword = "Pabiligamot10";
-$dbname = "u711816221_gorder";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "gorder";
 
 // Create connection
 $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
