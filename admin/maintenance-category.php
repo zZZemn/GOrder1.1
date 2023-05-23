f907de4707f793dea3c33017a90f74e325202df8
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-05-24 00:13:12 +0800

    toastr

--- admin/maintenance-category.php
+++ admin/maintenance-category.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['id'])) {
     <link rel="stylesheet" href="../css/maintenance-category.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="shortcut icon" href="../img/ggd-logo-plain.png" type="image/x-icon">
-    <link rel="stylesheet" href="../toastr-master/toastr.scss">
+    <link rel="stylesheet" href="../toastr/toastr.scss">
     <title>GOrder | Maintenance</title>
 </head>
 
@@ -422,7 +422,7 @@ if (isset($_SESSION['id'])) {
         <script src="../js/mess-send.js"></script>
         <script src="../js/mess-scroll.js"></script>
         <script src="../js/category-edit.js"></script>
-        <script src="../toastr-master/toastr.js"></script>
+        <script src="../toastr/toastr.js"></script>
 
     <?php else : ?>
         <div class="access-denied">
