9d83a8de3b82022d6e6ddbfbe5360c819e9b0376
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-05-29 23:02:49 +0800

    db confifg

--- database/db.php
+++ database/db.php
@@ -1,13 +1,13 @@
 <?php 
-// $dbservername = "localhost";
-// $dbusername = "u711816221_goldengate";
-// $dbpassword = "Pabiligamot10";
-// $dbname = "u711816221_gorder";
-
 $dbservername = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "gorder";
+$dbusername = "u711816221_goldengate";
+$dbpassword = "Pabiligamot10";
+$dbname = "u711816221_gorder";
+
+// $dbservername = "localhost";
+// $dbusername = "root";
+// $dbpassword = "";
+// $dbname = "gorder";
 
 // Create connection
 $conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
