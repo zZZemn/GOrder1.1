69f8f2b57ee4e5ef9c7807077c0d48965c28e952
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-05-31 16:01:32 +0800

    order status update

--- sales/order-details.php
+++ sales/order-details.php
@@ -10,10 +10,6 @@ if (isset($_SESSION['id'])) {
     $result  = $conn->query($sql);
     $emp = $result->fetch_assoc();
 
-
-    $emp_type = $emp['EMP_TYPE'];
-    $emp_status = $emp['EMP_STATUS'];
-
     if (isset($emp) && $emp["EMP_TYPE"] == "Admin" || $emp['EMP_TYPE'] == "PA" && $emp['EMP_STATUS'] == "active") {
         if (isset($_GET['transaction_id'])) {
             $transactionID = filter_input(INPUT_GET, 'transaction_id', FILTER_SANITIZE_STRING);
@@ -21,6 +17,8 @@ if (isset($_SESSION['id'])) {
             $orderDetails_result = $conn->query($orderDetails_sql);
             if ($orderDetails_result->num_rows > 0) {
                 $order = $orderDetails_result->fetch_assoc();
+                $order_status = $order['STATUS'];
+                $del_type = $order['DELIVERY_TYPE'];
 
 ?>
                 <!DOCTYPE html>
@@ -43,27 +41,35 @@ if (isset($_SESSION['id'])) {
 
                     <div class="top-contents-container">
                         <p class="transaction-id"><?php echo $order['TRANSACTION_ID'] ?></p>
-                        <div class="status-container">
-                            <div class="progress" style="height: 8px; width: 400px">
-                                <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                            <div class="progress-circle">
-                                <div class="circle bg-primary" style=" left: 48%;"><span class="bg-primary">value</span></div>
-                            </div>
-                            <div class="progress-photo-container">
-                                <i class="fa-solid fa-location-dot" id="waiting"></i>
-                                <i class="fa-solid fa-check" id="accepted"></i>
-                                <i class="fa-solid fa-box-open" id="for-delivery"></i>
-                                <i class="fa-solid fa-motorcycle" id="shipped"></i>
-                                <i class="fa-solid fa-house-circle-check" id="delivered"></i>
-                            </div>
+                        <input type="hidden" id="transaction_id_hidden" value="<?php echo $order['TRANSACTION_ID'] ?>">
+                        <div class="status-container" id="status_container">
+
                         </div>
+                        <?php if ($del_type === 'Deliver') { ?>
+                            <select class="form-control" id="update-order-status" <?php echo ($order_status === 'Delivered') ? 'disabled' : '' ?>>
+                                <?php
+                                if ($order_status === 'Delivered') {
+                                ?>
+                                    <option value="Delivered">Delivered</option>
+                                <?php
+                                } else {
+                                ?>
+                                    <option value="Waiting" <?php echo ($order_status === 'Waiting') ? 'selected' : '' ?>>Waiting</option>
+                                    <option value="Accepted" <?php echo ($order_status === 'Accepted') ? 'selected' : '' ?>>Accepted</option>
+                                    <option value="For-Delivery" <?php echo ($order_status === 'For-Delivery') ? 'selected' : '' ?>>For Delivery</option>
+                                    <option value="Shipped" <?php echo ($order_status === 'Shipped') ? 'selected' : '' ?>>Shipped</option>
+                            <?php
+                                }
+                            }
+                            ?>
+                            </select>
                     </div>
 
                     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
                     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
                     <script src="https://kit.fontawesome.com/c6c8edc460.js" crossorigin="anonymous"></script>
+                    <script src="../js/order-details-realtime.js"></script>
                 </body>
 <?php
 
