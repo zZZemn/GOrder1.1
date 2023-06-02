95e31400c2699d070867ac2c467977875d99873e
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-06-03 00:09:50 +0800

    invoice

--- sales/view-invoice.php
+++ sales/view-invoice.php
@@ -11,11 +11,11 @@ if (isset($_SESSION['id'])) {
 
     if (isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id'])) {
         $transactionID = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
-        $s_details_sql = "SELECT * FROM sales_details WHERE TRANSACTION_ID = $transactionID";
+        $s_details_sql = "SELECT *, SUM(QUANTITY) AS TOTAL_QUANTITY, SUM(AMOUNT) AS TOTAL_AMOUNT FROM sales_details WHERE TRANSACTION_ID = $transactionID GROUP BY PRODUCT_ID";
         $s_details_result = $conn->query($s_details_sql);
         if ($s_details_result->num_rows > 0) {
             $sales_sql = "SELECT * FROM sales WHERE TRANSACTION_ID = $transactionID";
-            $sales_result = $conn->query($sales_sql);     
+            $sales_result = $conn->query($sales_sql);
             if ($sales_result->num_rows > 0) {
                 $sales = $sales_result->fetch_assoc();
 
@@ -59,88 +59,103 @@ if (isset($_SESSION['id'])) {
                 <link rel="shortcut icon" href="../img/ggd-logo-plain.png" type="image/x-icon">
                 <title>GOrder | Invoice</title>
             </head>
-            <table class="invoice-container">
-                <tr class="heading">
-                    <td colspan="4"><center>Golden Gate Drugstore</center></td>
-                </tr>
-                <tr class="heading">
-                    <td colspan="4"><center>Patubig, Marilao, Bulacan</center></td>
-                </tr>
-                <tr class="heading">
-                    <td colspan="4"><center><?php echo $sales['DATE'] . " | " . $sales['TIME'] ?></center></td>
-                </tr>
-                <tr class="headers">
-                    <td>Product</td>
-                    <td>Price</td>
-                    <td>Quantity</td>
-                    <td>Amount</td>
-                </tr>
-                <?php
-                while ($row = $s_details_result->fetch_assoc()) {
-                    $productID = $row['PRODUCT_ID'];
-                    $product_sql = "SELECT * FROM products WHERE PRODUCT_ID = $productID";
-                    $product_result = $conn->query($product_sql);
-                    if ($product_result->num_rows > 0) {
-                        $product = $product_result->fetch_assoc();
-                    } else {
-                        echo '
-                            <head>
-                            <link rel="stylesheet" href="../css/access-denied.css">
-                            </head>
-                            <div class="access-denied">
-                                <h1>Access Denied</h1>
-                                <h5>Invalid to access this page. 3</h5>
-                            </div>';
-                        exit();
-                    }
-                ?>
-                    <tr>
-                        <td><?php echo $product['PRODUCT_NAME'] ?></td>
-                        <td><?php echo $product['SELLING_PRICE'] ?></td>
-                        <td class="qty"><?php echo $row['QUANTITY'] ?></td>
-                        <td><?php echo $row['AMOUNT'] ?></td>
-                    </tr>
-                <?php
-                }
-                ?>
-                <tr class="line">
-                    <td colspan="4"><hr></td>
-                </tr>
-                <tr>
-                    <td>Subtotal</td>
-                    <td>:</td>
-                    <td colspan="2" class="computed"><?php echo $sales['SUBTOTAL'] ?></td>
-                </tr>
-                <tr>
-                    <td>VAT</td>
-                    <td>:</td>
-                    <td colspan="2" class="computed"><?php echo $sales['VAT'] ?></td>
-                </tr>
-                <tr>
-                    <td>Discount</td>
-                    <td>:</td>
-                    <td colspan="2" class="computed"><?php echo $sales['DISCOUNT'] ?></td>
-                </tr>
-                <tr>
-                    <td>Total</td>
-                    <td>:</td>
-                    <td colspan="2" class="computed"><?php echo $sales['TOTAL'] ?></td>
-                </tr>
-                <tr>
-                    <td>Payment</td>
-                    <td>:</td>
-                    <td colspan="2" class="computed"><?php echo $sales['PAYMENT'] ?></td>
-                </tr>
-                <tr>
-                    <td>Change</td>
-                    <td>:</td>
-                    <td colspan="2" class="computed"><?php echo $sales['CHANGE'] ?></td>
-                </tr>
 
-                <tr class="process-by">
-                    <td colspan="4">Process By: <?php echo $emp_name ?> >>>>> Transaction ID: <?php echo $transactionID ?></td>
-                </tr>
-            </table>
+            <body>
+                <table class="invoice-container">
+                    <tr class="heading">
+                        <td colspan="5">
+                            <center>Golden Gate Drugstore</center>
+                        </td>
+                    </tr>
+                    <tr class="heading">
+                        <td colspan="5">
+                            <center>Patubig, Marilao, Bulacan</center>
+                        </td>
+                    </tr>
+                    <tr class="heading">
+                        <td colspan="5">
+                            <center><?php echo $sales['DATE'] . " | " . $sales['TIME'] ?></center>
+                        </td>
+                    </tr>
+                    <tr class="headers">
+                        <td>Product</td>
+                        <td>Price</td>
+                        <td>Quantity</td>
+                        <td>Amount</td>
+                        <td>Action</td>
+                    </tr>
+                    <?php
+                    while ($row = $s_details_result->fetch_assoc()) {
+                        $productID = $row['PRODUCT_ID'];
+                        $product_sql = "SELECT * FROM products WHERE PRODUCT_ID = $productID";
+                        $product_result = $conn->query($product_sql);
+                        if ($product_result->num_rows > 0) {
+                            $product = $product_result->fetch_assoc();
+                        } else {
+                            echo '
+                        <head>
+                        <link rel="stylesheet" href="../css/access-denied.css">
+                        </head>
+                        <div class="access-denied">
+                        <h1>Access Denied</h1>
+                        <h5>Invalid to access this page. 3</h5>
+                        </div>';
+                            exit();
+                        }
+                    ?>
+                        <tr>
+                            <td><?php echo $product['PRODUCT_NAME'] ?></td>
+                            <td><?php echo $product['SELLING_PRICE'] ?></td>
+                            <td class="qty"><?php echo $row['TOTAL_QUANTITY'] ?></td>
+                            <td><?php echo $row['TOTAL_AMOUNT'] ?></td>
+                            <td><a href="" class="btn-return"><i class="fa-solid fa-rotate-left"></i></a></td>
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                    <tr class="line">
+                        <td colspan="5">
+                            <hr>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Subtotal</td>
+                        <td>:</td>
+                        <td colspan="2" class="computed"><?php echo $sales['SUBTOTAL'] ?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">VAT</td>
+                        <td>:</td>
+                        <td colspan="2" class="computed"><?php echo $sales['VAT'] ?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Discount</td>
+                        <td>:</td>
+                        <td colspan="2" class="computed"><?php echo $sales['DISCOUNT'] ?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Total</td>
+                        <td>:</td>
+                        <td colspan="2" class="computed"><?php echo $sales['TOTAL'] ?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Payment</td>
+                        <td>:</td>
+                        <td colspan="2" class="computed"><?php echo $sales['PAYMENT'] ?></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Change</td>
+                        <td>:</td>
+                        <td colspan="2" class="computed"><?php echo $sales['CHANGE'] ?></td>
+                    </tr>
+
+                    <tr class="process-by">
+                        <td colspan="5">Process By: <?php echo $emp_name ?> >>>>> Transaction ID: <?php echo $transactionID ?></td>
+                    </tr>
+                </table>
+
+                <script src="https://kit.fontawesome.com/c6c8edc460.js" crossorigin="anonymous"></script>
+            </body>
 <?php
         } else {
             echo '
@@ -166,3 +181,4 @@ if (isset($_SESSION['id'])) {
     header("Location: ../index.php");
     exit();
 }
+?>
