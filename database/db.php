1fe53aba190b686e2d71b5fd8ca762ccd9a83d18
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-06-05 01:40:55 +0800

    db config

--- database/db.php
+++ database/db.php
@@ -1,8 +1,8 @@
 <?php 
 $dbservername = "localhost";
-$dbusername = "u711816221_goldengate_f";
+$dbusername = "u711816221_gorder";
 $dbpassword = "Pabiligamot10";
-$dbname = "u711816221_gorder_final";
+$dbname = "u711816221_goldengate";
 
 // $dbservername = "localhost";
 // $dbusername = "root";
