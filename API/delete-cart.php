35cd6acd2a1641d5bb9a1962640fe5b897e09ec4
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-06-14 19:20:00 +0800

    delete cart update to post

--- API/delete-cart.php
+++ API/delete-cart.php
@@ -1,32 +1,21 @@
 <?php 
 header('Acces-Control-Allow-Origin:*');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Method: GET');
+header('Access-Control-Allow-Method: POST');
 header('Access-Control-Allow-Headers: Content-Type, Address-Control-Allow-Headers, Autorization, X-Request-With');
 
 include('functions.php');
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-if($requestMethod == "GET")
+if($requestMethod == "POST")
 {
-    if(isset($_GET['cust_id']) && isset($_GET['product_id']))
-    {
-        $custID = $_GET['cust_id'];
-        $product_id = $_GET['product_id'];
+        $data = json_decode(file_get_contents("php://input"));
+        $custID = $data->cust_id;
+        $product_id = $data->product_id;
 
         $deleteCart = deleteCart($custID, $product_id);
         echo $deleteCart;
-    }
-    else
-    {
-        $data = [
-            'status' => 405,
-            'message' => 'Access Deny',
-        ];
-        header("HTTP/1.0 405 Access Deny");
-        echo json_encode($data);
-    }
 }
 else
 {
