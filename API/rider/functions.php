5e7be17c8b4188e3c6318848aca25b07dc3a37e6
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-06-25 14:58:45 +0800

    order details api

--- API/rider/functions.php
+++ API/rider/functions.php
@@ -84,16 +84,16 @@ function toDeliver($id)
         $rider = $rider_result->fetch_assoc();
         if ($rider['EMP_TYPE'] === 'Rider') {
 
-            $order_sql = "SELECT * FROM `order` WHERE RIDER_ID = '$id' AND DELIVERY_TYPE = 'Deliver' AND STATUS = 'For-Delivery'";
+            $order_sql = "SELECT * FROM `order` WHERE RIDER_ID = '$id' AND DELIVERY_TYPE = 'Deliver' AND (STATUS = 'For-Delivery' OR STATUS = 'Shipped')";
             $order_result = $conn->query($order_sql);
-            if($order_result->num_rows > 0){
+            if ($order_result->num_rows > 0) {
                 $orderlist = [];
-                while($order = $order_result->fetch_assoc()){
+                while ($order = $order_result->fetch_assoc()) {
                     $transaction_id = $order['TRANSACTION_ID'];
                     $cust_id = $order['CUST_ID'];
                     $user_result = $conn->query("SELECT * FROM customer_user WHERE CUST_ID = '$cust_id'");
                     $user = $user_result->fetch_assoc();
-                    $fullname = $user['FIRST_NAME'].' '.$user['LAST_NAME'];
+                    $fullname = $user['FIRST_NAME'] . ' ' . $user['LAST_NAME'];
 
                     $bgy_id = $order['BARANGAY_ID'];
 
@@ -117,31 +117,11 @@ function toDeliver($id)
                     $province_name = $province['PROVINCE'];
 
                     $region_result = $conn->query("SELECT REGION FROM region WHERE REGION_ID = '$region_id'");
-                    $region =$region_result->fetch_assoc();
-                    
+                    $region = $region_result->fetch_assoc();
+
                     $region_name = $region['REGION'];
 
-                    $full_address = $order['UNIT_STREET'].', '.$barangay_name.', '.$municipality_name.', '.$province_name.', '.$region_name;
-
-                    $order_details_result = $conn->query("SELECT * FROM order_details WHERE TRANSACTION_ID = '$transaction_id'");
-                    $order_details_array = [];
-                    if($order_details_result->num_rows > 0){
-                        while($order_details = $order_details_result->fetch_assoc()){
-                            $product_id = $order_details['PRODUCT_ID'];
-                            $product_result = $conn->query("SELECT PRODUCT_NAME, PRODUCT_IMG FROM products WHERE PRODUCT_ID = $product_id");
-                            $product = $product_result->fetch_assoc();
-                            $product_name = $product['PRODUCT_NAME'];
-                            $product_img = 'https://gorder.website/img/products/'.$product['PRODUCT_IMG'];
-
-                            $order_details_1 = [
-                                'product_name' => $product_name,
-                                'product_img' => $product_img,
-                                'qty' => intval($order_details['QTY']),
-                                'amount' => floatval($order_details['AMOUNT'])
-                            ];
-                            $order_details_array[] = $order_details_1;
-                        }
-                    }
+                    $full_address = $order['UNIT_STREET'] . ', ' . $barangay_name . ', ' . $municipality_name . ', ' . $province_name . ', ' . $region_name;
 
                     $orders = [
                         'transaction_id' => $transaction_id,
@@ -150,7 +130,6 @@ function toDeliver($id)
                         'payment_type' => $order['PAYMENT_TYPE'],
                         'address' => $full_address,
                         'total' => floatval($order['TOTAL']),
-                        'products' => $order_details_array
                     ];
 
                     $orderlist[] = $orders;
@@ -159,11 +138,10 @@ function toDeliver($id)
                 $data = [
                     'status' => 200,
                     'message' => 'All orders that you need to deliver asap',
-                    'orders'=> $orderlist
+                    'orders' => $orderlist
                 ];
                 header("HTTP/1.0 404 OK");
                 return json_encode($data);
-
             } else {
                 $data = [
                     'status' => 200,
@@ -172,7 +150,6 @@ function toDeliver($id)
                 header("HTTP/1.0 404 Not Found");
                 return json_encode($data);
             }
-
         } else {
             $data = [
                 'status' => 404,
@@ -185,6 +162,102 @@ function toDeliver($id)
         $data = [
             'status' => 404,
             'message' => 'No Emp Found',
+        ];
+        header("HTTP/1.0 404 Not Found");
+        return json_encode($data);
+    }
+}
+
+
+function deliverDetails($rider_id, $order_id)
+{
+    global $conn;
+    $order_result = $conn->query("SELECT * FROM `order` WHERE TRANSACTION_ID = '$order_id' AND RIDER_ID = '$rider_id' AND (STATUS = 'For-Delivery' OR STATUS = 'Shipped')");
+    if ($order_result->num_rows > 0) {
+        $order = $order_result->fetch_assoc();
+        $cust_id = $order['CUST_ID'];
+
+        $user_result = $conn->query("SELECT * FROM customer_user WHERE CUST_ID = '$cust_id'");
+        $user = $user_result->fetch_assoc();
+        $fullname = $user['FIRST_NAME'] . ' ' . $user['LAST_NAME'];
+
+        $bgy_id = $order['BARANGAY_ID'];
+
+        $barangay_result = $conn->query("SELECT MUNICIPALITY_ID, BARANGAY, DELIVERY_FEE FROM barangay WHERE BARANGAY_ID = '$bgy_id'");
+        $barangay = $barangay_result->fetch_assoc();
+
+        $muni_id = $barangay['MUNICIPALITY_ID'];
+        $barangay_name = $barangay['BARANGAY'];
+        $df = $barangay['DELIVERY_FEE'];
+
+        $municipality_result = $conn->query("SELECT PROVINCE_ID, MUNICIPALITY FROM municipality WHERE MUNICIPALITY_ID = '$muni_id'");
+        $municipality = $municipality_result->fetch_assoc();
+
+        $province_id = $municipality['PROVINCE_ID'];
+        $municipality_name = $municipality['MUNICIPALITY'];
+
+        $province_result = $conn->query("SELECT REGION_ID, PROVINCE FROM province WHERE PROVINCE_ID = '$province_id'");
+        $province = $province_result->fetch_assoc();
+
+        $region_id = $province['REGION_ID'];
+        $province_name = $province['PROVINCE'];
+
+        $region_result = $conn->query("SELECT REGION FROM region WHERE REGION_ID = '$region_id'");
+        $region = $region_result->fetch_assoc();
+
+        $region_name = $region['REGION'];
+
+        $full_address = $order['UNIT_STREET'] . ', ' . $barangay_name . ', ' . $municipality_name . ', ' . $province_name . ', ' . $region_name;
+
+        $order_details_result = $conn->query("SELECT * FROM order_details WHERE TRANSACTION_ID = '$order_id'");
+        $order_details_array = [];
+        if ($order_details_result->num_rows > 0) {
+            while ($order_details = $order_details_result->fetch_assoc()) {
+                $product_id = $order_details['PRODUCT_ID'];
+                $product_result = $conn->query("SELECT PRODUCT_NAME, PRODUCT_IMG FROM products WHERE PRODUCT_ID = $product_id");
+                $product = $product_result->fetch_assoc();
+                $product_name = $product['PRODUCT_NAME'];
+                $product_img = 'https://gorder.website/img/products/' . $product['PRODUCT_IMG'];
+
+                $order_details_1 = [
+                    'product_name' => $product_name,
+                    'product_img' => $product_img,
+                    'qty' => intval($order_details['QTY']),
+                    'amount' => floatval($order_details['AMOUNT'])
+                ];
+                $order_details_array[] = $order_details_1;
+            }
+            $orders = [
+                'transaction_id' => $order_id,
+                'order_status' => $order['STATUS'],
+                'cust_id' => $cust_id,
+                'cust_name' => $fullname,
+                'payment_type' => $order['PAYMENT_TYPE'],
+                'address' => $full_address,
+                'total' => floatval($order['TOTAL']),
+                'products' => $order_details_array
+            ];
+
+            $data = [
+                'status' => 200,
+                'message' => 'Order Fetch Success',
+                'order_details' => $orders
+            ];
+            header("HTTP/1.0 404 OK");
+            return json_encode($data);
+        } else {
+            $data = [
+                'status' => 200,
+                'message' => 'Order Empty'
+            ];
+            header("HTTP/1.0 404 OK");
+            return json_encode($data);
+            exit;
+        }
+    } else {
+        $data = [
+            'status' => 404,
+            'message' => 'Access Denied',
         ];
         header("HTTP/1.0 404 Not Found");
         return json_encode($data);
