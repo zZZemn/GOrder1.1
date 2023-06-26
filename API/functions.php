aba694561235e2ff241a5874e3029aa57c732e97
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-06-26 18:22:39 +0800

    checkout

--- API/functions.php
+++ API/functions.php
@@ -799,16 +799,18 @@ function checkout($id)
             $data = [
                 'status' => 200,
                 'message' => 'Computed Price',
-                'items' => $order_items_array,
-                'subtotal' => $subtotal,
-                'vat' => $vat,
-                'discount' => $discount,
-                'total' => $total,
-                'delivery_fee' => $df,
-                'total_plus_delivery_fee' => $total + $df,
+                'items' => [
+                    $order_items_array,
+                    'subtotal' => $subtotal,
+                    'vat' => $vat,
+                    'discount' => $discount,
+                    'total' => $total,
+                    'delivery_fee' => $df,
+                    'total_plus_delivery_fee' => $total + $df,
+                ]
             ];
-            header("HTTP/1.0 405 Access Deny");
-            return json_encode($data);
+            header("HTTP/1.0 200 OK");
+            echo json_encode($data);
         } else {
             $data = [
                 'status' => 405,
