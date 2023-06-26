68bd8bb967dbae7489aa91c958128da76ca15c56
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-06-26 23:16:01 +0800

    discount

--- ajax-url/add-discount.php
+++ ajax-url/add-discount.php
@@ -35,7 +35,7 @@ if (isset($_SESSION['id'])) {
         if ($conn->query($insert_discount_sql) === TRUE && $conn->query($add_discount_log) === TRUE) {
             echo "inserted";
         } else {
-            echo "invalid 1";
+            echo "invalid 1".$discount_percentage;
         }
     } else {
         echo "invalid";
