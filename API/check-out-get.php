3fba4994272e8b3d999b620fe1301c4b4eca94f4
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-09-17 13:58:03 +0800

    new check out

--- API/check-out-get.php
+++ API/check-out-get.php
@@ -9,11 +9,9 @@ include('functions.php');
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 if ($requestMethod == "GET") {
-    if (isset($_GET['id']) && isset($_GET['payment_type']) && isset($_GET['delivery_type'])) {
+    if (isset($_GET['id'])) {
         $id = $_GET['id'];
-        $payment_type = $_GET['payment_type'];
-        $delivery_type = $_GET['delivery_type'];
-        $order = checkout($id, $payment_type, $delivery_type);
+        $order = checkout($id);
         echo $order;
     } else {
         $data = [
--- API/check-out.php
+++ API/check-out.php
@@ -11,11 +11,9 @@ $requestMethod = $_SERVER['REQUEST_METHOD'];
 if ($requestMethod == "POST") {
     $data = json_decode(file_get_contents("php://input"));
 
-    if (isset($data->id) && isset($data->payment_type) && isset($data->delivery_type)) {
+    if (isset($data->id)) {
         $id = $data->id;
-        $payment_type = $data->payment_type;
-        $delivery_type = $data->delivery_type;
-        $order = checkout($id, $payment_type, $delivery_type);
+        $order = checkout($id);
         echo $order;
     } else {
         $data = [
