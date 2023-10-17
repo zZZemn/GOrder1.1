905b30ecfbeae6fb022343232e9426783c980f8e
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-10-17 16:49:53 +0800

    printing

--- sales/pos.php
+++ sales/pos.php
@@ -623,7 +623,7 @@ if (isset($_SESSION['id'])) {
         </div>
 
         <input type="hidden" id="personLoggedIn" value="<?= $emp['FIRST_NAME'] . " " . $emp["MIDDLE_INITIAL"] . " " . $emp['LAST_NAME'] ?>">
-        <p class="emptype-name remove-when-print">Printed By: <?php echo $emp['FIRST_NAME'] . " " . $emp["MIDDLE_INITIAL"] . " " . $emp['LAST_NAME'] ?></p>
+        <p class="emptype-name remove-when-print hide-me-final">Printed By: <?php echo $emp['FIRST_NAME'] . " " . $emp["MIDDLE_INITIAL"] . " " . $emp['LAST_NAME'] ?></p>
 
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
